c7583fe6665dacfdb6de5ccebcb0248e4f22cac8
Dudharejiya Kartik <137186473+Dudharejiya-Kartik@users.noreply.github.com>
2023-12-28 08:30:36 +0530

    Update main.dart

--- Ch_8/8_1/ch8_1_7/lib/main.dart
+++ Ch_8/8_1/ch8_1_7/lib/main.dart
@@ -1,40 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            title: Text(
+            title: const Text(
               "3D Cube",
               style: TextStyle(
-                fontSize: 25,
                 color: Colors.white,
+                fontSize: 23,
               ),
             ),
             centerTitle: true,
+            backgroundColor: Color(0xff009688),
           ),
           body: Align(
             child: Container(
-              height: 300,
-              width: 300,
+              height: 240,
+              width: 240,
               decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              alignment: Alignment.center,
-             child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  color: Colors.lightBlueAccent,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 6,
                 ),
-
               ),
-
-
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff009688),
+                  border: Border.symmetric(
+                    vertical: BorderSide(
+                      color : Color(0xff35A29F),
+                      width: 30,
+                    ),
+                    horizontal: BorderSide(
+                      color: Color(0xff52D7CA),
+                      width: 30,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
